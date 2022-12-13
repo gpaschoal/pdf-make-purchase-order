c766289b67ed50adea1ad003a71cc25fc3e7267d
guilherme <paschoal.gui@gmail.com>
2022-12-12 22:35:35 -0300

    adiciona melhorias

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import { Router, Request, Response } from 'express'
 import { PurchaseOrderUseCase } from './application/use-cases/reports-use-cases/purchase-order-use-case'
 
-const router = express.Router()
+const router = Router()
 
-router.post('/criar-pdf', async (req, res) => {
+router.post('/create-purchase-order', async (req: Request, res: Response) => {
   const usecase = new PurchaseOrderUseCase()
-  await usecase.handle({})
+  await usecase.handle(req.body)
   res.send()
 })
 
